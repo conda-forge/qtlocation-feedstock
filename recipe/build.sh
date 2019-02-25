c004c066225c27a630583edd67ad9324dfb3f327
Chris Holden <ceholden@gmail.com>
2019-02-24 19:18:48 -0500

    Fix build for Qt issue & add CDTs

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,20 @@
 [[ -d build ]] || mkdir build
 cd build/
 
-qmake ../qtlocation.pro
+# Need to specify these QMAKE variables because of build environment residue
+# in qt mkspecs referencing "qt_1548879054661"
+# https://github.com/conda-forge/qtlocation-feedstock/pull/3#issuecomment-466278804
+qmake \
+    QMAKE_LIBDIR=/usr/lib64 \
+    QMAKE_INCDIR=/usr/include \
+    QMAKE_CC=${CC} \
+    QMAKE_CXX=${CXX} \
+    QMAKE_LINK=${CXX} \
+    QMAKE_RANLIB=${RANLIB} \
+    QMAKE_OBJDUMP=${OBJDUMP} \
+    QMAKE_STRIP=${STRIP} \
+    QMAKE_AR="${AR} cqs" \
+    ../qtlocation.pro
 
 # Log files on Travis CI are limited to 4MB and this step usually exceeds
 # that limit. Removes ability to diagnose, but lets the build complete.
@@ -23,7 +36,17 @@ echo "Building examples to test library install"
 mkdir -p examples
 cd examples/
 
-qmake ../../examples/examples.pro
+qmake \
+    QMAKE_LIBDIR=/usr/lib64 \
+    QMAKE_INCDIR=/usr/include \
+    QMAKE_CC=${CC} \
+    QMAKE_CXX=${CXX} \
+    QMAKE_LINK=${CXX} \
+    QMAKE_RANLIB=${RANLIB} \
+    QMAKE_OBJDUMP=${OBJDUMP} \
+    QMAKE_STRIP=${STRIP} \
+    QMAKE_AR="${AR} cqs" \
+    ../../examples/examples.pro
 
 if [ $(uname) == Darwin ]; then
     echo "Silencing make step while running on Travis-CI"
