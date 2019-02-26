b8c5213b815c11ddc07a34a4fb213e5001542f02
Chris Holden <ceholden@gmail.com>
2019-02-25 21:09:45 -0500

    Show build output on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,18 +18,9 @@ qmake \
     QMAKE_AR="${AR} cqs" \
     ../qtlocation.pro
 
-# Log files on Travis CI are limited to 4MB and this step usually exceeds
-# that limit. Removes ability to diagnose, but lets the build complete.
-if [ $(uname) == Darwin ]; then
-    echo "Silencing make step while running on Travis-CI"
-    make -j$CPU_COUNT > /dev/null
-    make check > /dev/null
-    make install
-else
-    make -j$CPU_COUNT
-    make check
-    make install
-fi
+make -j$CPU_COUNT
+make check
+make install
 
 # Try building "examples/" as a test
 echo "Building examples to test library install"
@@ -48,11 +39,5 @@ qmake \
     QMAKE_AR="${AR} cqs" \
     ../../examples/examples.pro
 
-if [ $(uname) == Darwin ]; then
-    echo "Silencing make step while running on Travis-CI"
-    make -j$CPU_COUNT > /dev/null
-    make check > /dev/null
-else
-    make -j$CPU_COUNT
-    make check
-fi
+make -j$CPU_COUNT > /dev/null
+make check > /dev/null
